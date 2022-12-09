cec1b4e2db2788ec6a07079a7acdd84203e9a74b
phob0z <leonel.alfonso@gmail.com>
2022-12-09 16:50:13 -0500

    Geolocalizacion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Geolocation } from '@awesome-cordova-plugins/geolocation/ngx';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,30 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  constructor(
+    private geolocation: Geolocation,
+  ) {
+    this.getGeolocation();
+  }
+
+  getGeolocation() {
+    this.geolocation.getCurrentPosition().then((resp) => {
+      console.log("Latitud:", resp.coords.latitude);
+      console.log("Longitud:", resp.coords.longitude);
+
+      
+      // resp.coords.latitude
+      // resp.coords.longitude
+    }).catch((error) => {
+      console.log('Error getting location', error);
+    });
+
+    // let watch = this.geolocation.watchPosition();
+    // watch.subscribe((data) => {
+    //   // data can be a set of coordinates, or an error (if an error occurred).
+    //   // data.coords.latitude
+    //   // data.coords.longitude
+    // });
+  }
 }
+
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
+
+
 export class HomePage {
 
-  constructor() {}
+  // constructor() {}
+  @Input() latitude = "";
+  // @Input() longitude = "";
 
 }
